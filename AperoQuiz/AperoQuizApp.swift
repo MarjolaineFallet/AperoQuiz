48ffe3c34a641a2c3678c877949d8a6b65bb6992
NathanFallet <contact@nathanfallet.me>
2023-07-24 20:26:31 +0200

    Initial Commit

---
+++ AperoQuiz/AperoQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  AperoQuizApp.swift
+//  AperoQuiz
+//
+//  Created by Nathan Fallet on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AperoQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
